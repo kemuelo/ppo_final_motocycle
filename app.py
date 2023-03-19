b2797ed18e2b81a9205c8026506d17e80fd00f07
kemuelo <nkk1703nkk@gmail.com>
2023-03-19 14:43:04 +0300

    seventh

--- app.py
+++ app.py
@@ -10,13 +10,17 @@ migrate = Migrate(app,db)
 
 class Pokupki(db.Model):
     __tablename__ = 'pokupki'
-    id = db.Column(db.Integer, primary_key = True)
-    oxygen = db.Column(db.Integer, primary_key = True)
-    fuel = db.Column(db.Integer, primary_key = True)
+    day = db.Column(db.Integer, primary_key = True)
+    oxygen = db.Column(db.Integer)
+    fuel = db.Column(db.Integer)
+    money = db.Column(db.Integer)
+    energy_distribution = db.Column(db.Integer)
 
-    def __init__(self, oxygen, fuel):
+    def __init__(self, oxygen, fuel, money, energy_distribution):
         self.oxygen = oxygen
         self.fuel = fuel
+        self.money = money
+        self.energy_distribution = energy_distribution
     def __repr__(self):
         return '<Pokupki %r>' % self.username
 
