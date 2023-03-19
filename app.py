a4e21e4e8698942601f7e8ebc6d00eff83323013
kemuelo <nkk1703nkk@gmail.com>
2023-03-19 13:16:24 +0300

    third

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from flask import Flask
-from config import Configuration
 from flask_migrate import Migrate
+from config import Configuration
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config.from_object(Configuration)
+
+db = SQLAlchemy(app)
+migrate = Migrate(app,db)
+
+
+
+
+class Roads(db.Model):
+    pass
