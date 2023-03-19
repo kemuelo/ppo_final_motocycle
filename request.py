f6abbb1e9bbe46d324754572e9a9f80f383aedaa
kemuelo <nkk1703nkk@gmail.com>
2023-03-19 14:01:05 +0300

    fifth

--- request.py
+++ request.py
@@ -1,6 +1,6 @@
 import requests
 import json
 
-ret = requests.get(f'https://dt.miet.ru/ppo_it_final', headers={'X-Auth-Token' : 'rr6jtsww'})
+ret = requests.get(f'https://dt.miet.ru/ppo_it_final', headers={'X-Auth-Token' : 'htbpme25'})
 
 print(f'Status code: {ret.status_code}\nText     : {ret.text}')
